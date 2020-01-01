94230b86186db6428082a44296f3d73033ac433f
Lucas Leite <lucasboleite@gmail.com>
2020-01-01 14:34:41 -0300

    Update main.sh
    Changed image to Unbox's logo

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export GITHUB_BRANCH=${GITHUB_REF##*heads/}
-export SLACK_ICON=${SLACK_ICON:-"https://avatars0.githubusercontent.com/u/43742164"}
+export SLACK_ICON=${SLACK_ICON:-"https://unbox-logo-images.s3.amazonaws.com/logo_box.png"}
 export SLACK_USERNAME=${SLACK_USERNAME:-"rtBot"}
 export CI_SCRIPT_OPTIONS="ci_script_options"
 export SLACK_TITLE=${SLACK_TITLE:-"Message"}
